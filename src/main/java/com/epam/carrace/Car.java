eea9db61eb51b3710d5b4feca4b7c2eaa56e8fe2
nazarevy4 <nazar.stukalo@gmail.com>
2018-08-26 23:18:23 +0300

    add car

---
+++ src/main/java/com/epam/carrace/Car.java
@@ -0,0 +1,39 @@
+package com.epam.carrace;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Car {
+
+    private List<Integer> speedPerSecond = new ArrayList<Integer>();
+    private int carNumber;
+
+    public List getSpeedPerSecond() {
+        return speedPerSecond;
+    }
+
+    public void setSpeedPerSecond(List speedPerSecond) {
+        this.speedPerSecond = speedPerSecond;
+    }
+
+    public void addSpeedPerSecond(int speedPerSecond) {
+        this.speedPerSecond.add(speedPerSecond);
+    }
+
+    public double getAvarageSpeed(){
+        int sum = 0;
+        for (Integer sps: speedPerSecond){
+            sum = + sps;
+        }
+
+        return sum/speedPerSecond.size();
+    }
+
+    public int getCarNumber() {
+        return carNumber;
+    }
+
+    public void setCarNumber(int carNumber) {
+        this.carNumber = carNumber;
+    }
+}
