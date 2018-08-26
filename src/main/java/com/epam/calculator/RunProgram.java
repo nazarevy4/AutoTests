62cda0625c4609cd89535dab094742ad27c95f37
nazarevy4 <nazar.stukalo@gmail.com>
2018-08-26 23:06:27 +0300

    first commit

---
+++ src/main/java/com/epam/calculator/RunProgram.java
@@ -0,0 +1,12 @@
+package com.epam.calculator;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class RunProgram {
+    public static void main(String[] args) throws IOException {
+        Logic newCalc = new Logic();
+        newCalc.startProgram();
+//        some comment
+    }
+}
